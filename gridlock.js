92ed8bbb60073edc80ebb3fd20e2c210867e9709
Ismail Badawi <ismail@badawi.io>
2019-09-08 19:58:01 -0400

    Add basic mouse movement.
    There's a bug with moving down, will fix.

--- gridlock.js
+++ gridlock.js
@@ -89,7 +89,7 @@ class Level {
     );
   }
 
-  move(x, y, by) {
+  moveByOne(x, y, by) {
     if (by != 1 && by != -1) {
       throw Error('can only move by 1');
     }
@@ -113,6 +113,97 @@ class Level {
       piece.y += by;
     }
   }
+
+  move(x, y, toX, toY) {
+    let piece = this.pieceAt(x, y);
+    if (!piece) {
+      throw Error('no piece at ' + JSON.stringify({x: x, y: y}));
+    }
+
+    if (piece.orientation == HORIZONTAL) {
+      if (y != toY) {
+        throw Error('stay in same lane horizontal');
+      }
+      for (let i = 0; i < Math.abs(toX - x); ++i) {
+        this.moveByOne(x, y, toX >= x ? 1 : -1);
+      }
+    } else if (piece.orientation == VERTICAL) {
+      if (x != toX) {
+        throw Error('stay in same lane vertical');
+      }
+      for (let i = 0; i < Math.abs(toY - y); ++i) {
+        this.moveByOne(x, y, toY >= y ? 1 : -1);
+      }
+    }
+  }
+}
+
+const CELL_SIZE = 100;
+
+class Game {
+  constructor(canvas, level) {
+    this.canvas = canvas;
+    this.level = level;
+
+    this.onMouseUp();
+  }
+
+  draw() {
+    let colors = [
+      'green',
+      'blue',
+      'cyan',
+      'yellow',
+      'magenta',
+      'pink',
+      'orange',
+      'brown',
+    ];
+    let nextColor = 0;
+
+    let ctx = this.canvas.getContext('2d');
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    for (let piece of this.level.pieces) {
+      let x = piece.x * CELL_SIZE;
+      let y = piece.y * CELL_SIZE;
+      let width = piece.orientation == VERTICAL ? CELL_SIZE : piece.size * CELL_SIZE;
+      let height = piece.orientation == HORIZONTAL ? CELL_SIZE : piece.size * CELL_SIZE;
+
+      ctx.fillStyle = piece.main ? 'red' : colors[nextColor++];
+      ctx.strokeStyle = 'black';
+      ctx.fillRect(x, y, width, height);
+      ctx.strokeRect(x, y, width, height);
+    }
+  }
+
+  onMouseDown(e) {
+    let x = Math.floor(e.clientX / CELL_SIZE);
+    let y = Math.floor(e.clientY / CELL_SIZE);
+    this.selectedPiece = this.level.pieceAt(x, y);
+  }
+
+  onMouseUp(e) {
+    this.selectedPiece = null;
+  }
+
+  onMouseMove(e) {
+    if (this.selectedPiece == null) {
+      return;
+    }
+
+    let fromX = this.selectedPiece.x;
+    let fromY = this.selectedPiece.y;
+
+    let toX = Math.floor(e.clientX / CELL_SIZE);
+    let toY = Math.floor(e.clientY / CELL_SIZE);
+
+    try {
+      this.level.move(fromX, fromY, toX, toY);
+      this.draw();
+    } catch (e) {
+    }
+  }
 }
 
 function startGame() {
@@ -130,29 +221,10 @@ function startGame() {
     'X...Z.',
   ].join('\n'))
 
-  let colors = [
-    'green',
-    'blue',
-    'cyan',
-    'yellow',
-    'magenta',
-    'pink',
-    'orange',
-    'brown',
-  ];
-  let nextColor = 0;
+  let game = new Game(canvas, level);
 
-  let ctx = canvas.getContext('2d');
-
-  for (let piece of level.pieces) {
-    let x = piece.x * 100;
-    let y = piece.y * 100;
-    let width = piece.orientation == VERTICAL ? 100 : piece.size * 100;
-    let height = piece.orientation == HORIZONTAL ? 100 : piece.size * 100;
-
-    ctx.fillStyle = piece.main ? 'red' : colors[nextColor++];
-    ctx.strokeStyle = 'black';
-    ctx.fillRect(x, y, width, height);
-    ctx.strokeRect(x, y, width, height);
-  }
+  game.draw(canvas);
+  canvas.addEventListener('mousedown', game.onMouseDown.bind(game), false);
+  canvas.addEventListener('mouseup', game.onMouseUp.bind(game), false);
+  canvas.addEventListener('mousemove', game.onMouseMove.bind(game), false);
 }
